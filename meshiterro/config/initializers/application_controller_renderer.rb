0802faff78423f0d8489f62d9c0dbed0f30f1d5b
Shunya Sago <shun.s0029@gmail.com>
2021-08-03 05:06:47 +0000

    [Finish] 4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5acc0af8c11c280cbfc6d40119850ec93cc8407f236586fab2c6a8ffa30329c9537f7aa3176e439283cc331dffb1ab365900804694e822e3c6099620c2203c3e'
